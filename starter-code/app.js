6dfc9db34ebbc2ed7ffb80c65316a1b1454baaf7
Shubhankar <shubhosen98@gmail.com>
2021-09-07 18:47:54 +0530

    Added toggle changes

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,4 @@
 //selecting the elements
-
 const image = document.querySelector('.js-split');
 const avatarImg = document.querySelector('.rounded-circle');
 const header = document.querySelector('.h2');
@@ -7,7 +6,7 @@ const versionLight = document.querySelector('.version-lg');
 const versionDark = document.querySelector('.version-dk');
 const inputBox = document.querySelector('.form-control');
 const searchIcon = document.querySelector('#basic-addon1');
-const searchButton = document.querySelector('.btn-js'); 
+const searchButton = document.querySelector('.btn-js');
 const lastRow = document.querySelector('.last-row');
 const desclastRow = document.querySelector('.desc-lst-row');
 const updateDate = document.querySelector('.update');
@@ -15,6 +14,8 @@ const Loremcontent = document.querySelectorAll('.lor-content');
 const profileInfo = document.querySelectorAll('.profile-info');
 const btnClickable = document.querySelector('.btn-sm');
 const footerImages = document.querySelectorAll('.svg-footer-img');
+const spansInfo = document.querySelectorAll('.d-block');
+
 
 
 // manipulating elements
@@ -23,9 +24,32 @@ btnClickable.addEventListener('click', async (evt) => {
 
     evt.preventDefault();
 
-    const data = await axios.get('https://api.github.com/users/Shubhankar-Sengupta');
-    Loremcontent[0].innerHTML = data.data.bio;
-    // avatarImg.src = data.data.avatar_url;
+    try {
+        const inpValue = inputBox.value;
+        const data = await axios.get(`https://api.github.com/users/${inpValue}`);
+
+
+        const bioData = data.data.bio;
+        const repos = data.data.public_repos;
+        const followers = data.data.followers;
+        const following = data.data.following;
+
+        if (bioData === '') {
+            "profile has no bio";
+        }
+        else Loremcontent[0].innerHTML = bioData;
+
+        spansInfo[0].innerHTML = repos;
+        spansInfo[1].innerHTML = followers;
+        spansInfo[2].innerHTML = following;
+    }
+
+
+    catch (err) {
+        "";
+    }
+
+    console.dir(inputBox);
 
 });
 
@@ -36,13 +60,14 @@ function toggleContent() {
     header.classList.toggle('header');
     versionDark.classList.toggle('display-dark');
     versionLight.classList.toggle('display-light');
-    inputBox.classList.toggle('frm-control-add', 'input-box');
+    inputBox.classList.toggle('frm-control-add');
+    inputBox.classList.toggle('control-back');
     searchIcon.classList.toggle('search-icon-background');
     lastRow.classList.toggle('last-row-adjustments');
     updateDate.classList.toggle('date-update');
     searchButton.classList.toggle('search-button');
     desclastRow.classList.toggle('desc-lst-rowColor');
-    // image.classList.toggle('image-src-toggle');
+    image.classList.toggle('icon-src-alt');
 
 
     for (let cont of Loremcontent) {
@@ -55,39 +80,21 @@ function toggleContent() {
         prof.children[0].classList.toggle('prof-color');
     }
 
-    // document.body.style.backgroundColor = `var(--dark-blue-black)`;
-    // header.style.color = `var(--dark-pure-white)`;
-    
-    // versionDark.style.display = `none`;
-    // versionLight.style.display = `inline-block`;
-    // versionLight.style.color = `var(--dark-pure-white)`;
-    // inputBox.style.backgroundColor = `var(--dark-blue)`;
-    // inputBox.classList.add('frm-control-add');
-    // searchIcon.style.backgroundColor = `var(--dark-blue)`;
-    // lastRow.style.backgroundColor = `var(--dark-blue)`;
-    // lastRow.style.color = `var(--dark-pure-white)`;
-    // updateDate.style.color = `var(--dark-pure-white)`;
 
 
-    // for (let svg of footerImages) {
-    //     svg.firstElementChild.attributes.fill.nodeValue = `#fff`;
-    // }
-    
+    for (let svg of footerImages) {
 
-    // for (let cont of Loremcontent) {
-    //     cont.style.color = `var(--dark-pure-white)`;
-    // }
+        let container = svg.firstElementChild.attributes.fill;
 
-    // for (let prof of profileInfo) {
-    //     prof.style.color = `var(--dark-pure-white)`;
-    //     prof.children[0].style.color = `var(--dark-pure-white)`;
-    // }
+        if (container.nodeValue === "#4b6a9b") {
+            container.nodeValue = '#fff';
+        }
 
-    // image.src = `file:///F:/FrontEndMentorChallange4/github-user-search-app/starter-code/assets/icon-sun.svg`;
-    // searchButton.style.backgroundColor = `var(--dark-blue)`;
-    // desclastRow.style.backgroundColor = `var(--dark-blue-black)`;
-    // inputBox.style.color =`var(--dark-pure-white)`;
-    // document.body.style.transition = `all 1s ease-out`;
+        else {
+            container.nodeValue = "#4b6a9b";
+        }
+
+    }
 
 }
 
